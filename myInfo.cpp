ab62ce1e03cac24107504064df52776defe10525
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-07-02 09:50:30 +0600

    Create myInfo.cpp

---
+++ myInfo.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  cout << "This is my GitHub Profile README" << endl;
+}
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t = 1;
+  while(t--) {
+    solve();
+ 
+  }
+
+  return 0;
+}
